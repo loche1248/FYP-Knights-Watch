384401c82c087a296a6545c8f36cc8ecf6eea4c2
loche1248 <lukeroche.2011@gmail.com>
2022-04-13 00:38:25 +0100

    Level 1 - 4x4 made + rooms are generating

---
+++ FYP Knight's Watch/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    public GameObject[] rooms;
+
+    private int direction;
+    public float moveAmount;
+
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = 0.25f;
+
+    private void Start()
+    {
+        int randStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    private void Update()
+    {
+        if (timeBtwRoom <= 0)
+        {
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        if (direction == 1 || direction == 2)
+        {
+            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+            transform.position = newPos;
+        } else if (direction == 3 || direction == 4)
+        {
+            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+            transform.position = newPos;
+        } else if (direction == 5)
+        {
+            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+            transform.position = newPos;
+        }
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 6);
+    }
+}
