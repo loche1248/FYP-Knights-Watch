ad1a32b7c3f720e94fe93b8fc5f5bb7e650a04eb
loche1248 <lukeroche.2011@gmail.com>
2022-04-17 18:00:48 +0100

    Update

--- FYP Knight's Watch/Assets/Scripts/KnightMovement.cs
+++ FYP Knight's Watch/Assets/Scripts/KnightMovement.cs
@@ -6,23 +6,50 @@ using UnityEngine;
 public class KnightMovement : MonoBehaviour
 {
 
-    public KnightController controller;
+    public CharacterControllerKW controller;
+    public Animator animator;
+
 
     public float runSpeed = 40f;
-    float horizontalMove = 0f;
 
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
-
-
-  
 }
+
 
